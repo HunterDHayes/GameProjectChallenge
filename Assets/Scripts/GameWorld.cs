d4bd65f80ce46cc11bc413da2fb956a234ce96b5
Matthew Salow <matthewsalow@fullsail.edu>
2015-03-18 18:34:11 -0400

    wsadsefdrg;l'

--- Assets/Scripts/GameWorld.cs
+++ Assets/Scripts/GameWorld.cs
@@ -4,16 +4,36 @@ using System.Collections;
 public class GameWorld : MonoBehaviour {
 	public GameObject player;	
 	public float worldSpeed;
+	public float spawnTime;
+	private float spawnTimer;
+	public GameObject PickUp;
+	public GameObject Obstacle;
 	// Use this for initialization
 	void Start () {
 		//init world
+		spawnTimer = spawnTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (new Vector3 (-worldSpeed * Time.deltaTime, 0, 0));
-		//for (int i = 0; i < GetComponentInChildren<Transform>(); i++) {
-		//
-		//}
+		spawnTimer -= Time.deltaTime;
+		if (spawnTimer < 0.0f) {
+			//spawn an item
+			if (Random.Range(0,1) > 0) {
+				//spawn pick up
+				GameObject newPickUp = Instantiate<GameObject>(PickUp);
+				//set position
+				newPickUp.transform.Translate(new Vector3(4,0,0));
+				newPickUp.transform.SetParent(transform);
+			} else {
+				//spawn obstacle
+				GameObject newObstacle = Instantiate<GameObject>(Obstacle);
+				//set position
+				newObstacle.transform.Translate(new Vector3(4,0,0));
+				newObstacle.transform.SetParent(transform);
+			}
+			spawnTimer = spawnTime;
+		}
 	}
 }
