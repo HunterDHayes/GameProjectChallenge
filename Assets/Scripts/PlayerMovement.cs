2f459d678de17bd3f17a5e7e0a67c2e8d36a0c8a
Matthew Salow <matthewsalow@fullsail.edu>
2015-03-18 15:17:52 -0400

    adding GamePlay.unity to the scenes and adding PlayerMovement.cs also adding the cloud.prefab and tempPlane.png

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float liftForce;
+	private Rigidbody2D rigid;
+	private Collider2D col;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+		col = GetComponent<Collider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//input
+
+		if (Input.GetMouseButtonDown(0)) {
+			rigid.AddForce(new Vector2(0,(liftForce*Time.deltaTime)));
+		}
+			
+	}
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log ("hit something");
+	}
+
+	void OnTriggerEnter2D(Collider2D collided){
+		if (collided.tag == "Cloud") {
+			Debug.Log ("hit a cloud");
+		} else {
+			Debug.Log ("triggered something");
+		}
+	}
+}
